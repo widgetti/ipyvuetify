480ef878b18b1d49c50b2b7857dfb23ae0794a71
Mario Buikhuizen <mbuikhuizen@gmail.com>
2019-04-10 15:14:08 +0200

    Make generated python classes available in main package

--- ipyvuetify/__init__.py
+++ ipyvuetify/__init__.py
@@ -1,6 +1,7 @@
 from ._version import version_info, __version__
 from .Html import Html
 from .VuetifyTemplate import VuetifyTemplate
+from .generated import *
 
 def _jupyter_nbextension_paths():
     return [{
