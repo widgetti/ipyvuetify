402f8f9e66ce0cac9fbe688e0983becfc4024c93
Mario Buikhuizen <mbuikhuizen@gmail.com>
2019-04-12 16:26:17 +0200

    Include helper functions in View classes.

--- js/lib/VuetifyTemplateView.js
+++ js/lib/VuetifyTemplateView.js
@@ -19,72 +19,73 @@ export class VuetifyTemplateView extends DOMWidgetView {
             this.vueApp = new Vue({
                 el: this.el,
 
-                render(createElement) {
+                render: createElement => {
                     // TODO: Don't use v-app in embedded mode
                     /* Prevent re-rendering of toplevel component. This happens on button-click in v-menu */
                     if (!this.ipyvuetify_app) {
-                        this.ipyvuetify_app = createElement("v-app", [createElement(createComponentObject(model))]);
+                        this.ipyvuetify_app = createElement("v-app", [createElement(this.createComponentObject(model))]);
                     }
                     return this.ipyvuetify_app;
                 }
             });
         });
     }
-}
 
-function createComponentObject(model) {
-    return {
-        data() {
-            return createDataMapping(model);
-        },
-        created() {
-            addModelListeners(model, this);
-        },
-        watch: createWatches(model),
-        methods: createMethods(model),
-        components: createComponents(model.get("components") || {}),
-        template: model.get("template"),
+    createComponentObject(model) {
+        const widgetView = this;
+        return {
+            data() {
+                return widgetView.createDataMapping(model);
+            },
+            created() {
+                widgetView.addModelListeners(model, this);
+            },
+            watch: this.createWatches(model),
+            methods: this.createMethods(model),
+            components: this.createComponents(model.get("components") || {}),
+            template: model.get("template"),
+        }
     }
-}
 
-function createDataMapping(model) {
-    return model.keys()
-        .filter(prop => !prop.startsWith("_") && !["events", "template", "components"].includes(prop))
-        .reduce((result, prop) => {
-            result[prop] = model.get(prop);
+    createDataMapping(model) {
+        return model.keys()
+            .filter(prop => !prop.startsWith("_") && !["events", "template", "components"].includes(prop))
+            .reduce((result, prop) => {
+                result[prop] = model.get(prop);
+                return result;
+            }, {});
+    }
+
+    addModelListeners(model, vueModel) {
+        model.keys()
+            .filter(prop => !prop.startsWith("_") && !["v_model", "components"].includes(prop))
+            .forEach(prop => model.on("change:" + prop, () => vueModel[prop] = model.get(prop)));
+    }
+
+    createWatches(model) {
+        return model.keys()
+            .filter(prop => !prop.startsWith("_") && !["events", "template", "components"].includes(prop))
+            .reduce((result, prop) => {
+                result[prop] = function (value) {
+                    model.set(prop, value === undefined ? null : value);
+                    model.save_changes();
+                };
+                return result;
+            }, {});
+    }
+
+    createMethods(model) {
+        return model.get("events").reduce((result, event) => {
+            result[event] = (value) => model.send({event, data: value});
             return result;
-        }, {});
-}
+        }, {})
+    }
 
-function addModelListeners(model, component) {
-    model.keys()
-        .filter(prop => !prop.startsWith("_") && !["v_model", "components"].includes(prop))
-        .forEach(prop => model.on("change:" + prop, () => component[prop] = model.get(prop)));
-}
-
-function createWatches(model) {
-    return model.keys()
-        .filter(prop => !prop.startsWith("_") && !["events", "template", "components"].includes(prop))
-        .reduce((result, prop) => {
-            result[prop] = function (value) {
-                model.set(prop, value === undefined ? null : value);
-                model.save_changes();
-            };
-            return result;
-        }, {});
-}
-
-function createMethods(model) {
-    return model.get("events").reduce((result, event) => {
-        result[event] = (value) => model.send({event, data: value});
-        return result;
-    }, {})
-}
-
-function createComponents(components) {
-    return Object.entries(components)
-        .reduce((result, [name, model]) => {
-            result[name] = createComponentObject(model);
-            return result;
-        }, {});
+    createComponents(components) {
+        return Object.entries(components)
+            .reduce((result, [name, model]) => {
+                result[name] = this.createComponentObject(model);
+                return result;
+            }, {});
+    }
 }
--- js/lib/VuetifyView.js
+++ js/lib/VuetifyView.js
@@ -18,180 +18,181 @@ export class VuetifyView extends DOMWidgetView {
             const model = this.model;
             this.vueApp = new Vue({
                 el: this.el,
-                render(createElement) {
+                render: createElement => {
                     // TODO: Don't use v-app in embedded mode
+                    /* Prevent re-rendering of toplevel component. This happens on button-click in v-menu */
                     if (!this.ipyvuetify_app) {
-                        this.ipyvuetify_app = createElement("v-app", [vueRender(createElement, model)]);
+                        this.ipyvuetify_app = createElement("v-app", [this.vueRender(createElement, model)]);
                     }
                     return this.ipyvuetify_app;
                 }
             });
         });
     }
-}
 
-function vueRender(createElement, model) {
-    let tag = model.getVuetifyTag();
-    if (tag === "html") {
-        return createElement(
-            model.get("tag"), {
-                ...model.get("vstyle") && {style: model.get("vstyle")},
-                ...model.get("vclass") && {class: model.get("vclass")},
-                ...model.get("attributes"),
-                ...model.get("slot") && {slot: model.get("slot")}
-            },
-            model.get("children").map(child => (typeof child === "string") ? child : vueRender(createElement, child)));
-    }
-    const elem = createElement({
-        data() {
-            return {
-                v_model: model.get("v_model")
-            };
-        },
-        created() {
-            addListeners(model, this);
-        },
-        render(createElement) {
+    vueRender(createElement, model) {
+        const widgetView = this;
+        let tag = model.getVuetifyTag();
+        if (tag === "html") {
             return createElement(
-                tag,
-                createContent(model, this),
-                renderChildren(this, model, createElement));
-        }
-    }, {...model.get("slot") && {slot: model.get("slot")}});
-
-    /* Impersonate the wrapped component (e.g. v-tabs uses this name to detect v-tab en v-tab-item) */
-    elem.componentOptions.Ctor.options.name = tag;
-    return elem;
-}
-
-function eventToObject(event) {
-    let props = undefined;
-    switch (event.constructor) {
-        case MouseEvent:
-            props = ["altKey", "ctrlKey", "metaKey", "shiftKey", "offsetX", "offsetY", "clientX", "clientY", "pageX", "pageY", "screenX", "screenY", "shiftKey", "x", "y"];
-            break;
-        case WheelEvent:
-            props = ["altKey", "ctrlKey", "metaKey", "shiftKey", "offsetX", "offsetY", "clientX", "clientY", "pageX", "pageY", "screenX", "screenY", "shiftKey", "x", "y", "wheelDelta", "wheelDeltaX", "wheelDeltaY"]
-    }
-
-    if (!props) {
-        return event;
-    }
-
-    return props.reduce(
-        (result, key) => {
-            result[key] = event[key];
-            return result},
-        {});
-}
-
-function registerChangedEvents(model, component) {
-
-    const previous = model.previous("_events") || [];
-    const current = model.get("_events");
-
-    const removed = previous.filter(e => !current.includes(e));
-    const added = current.filter(e => !previous.includes(e));
-
-    removed.forEach(e => component.$children[0].$off(e));
-    added.forEach(name => {
-        component.$children[0].$on(name, (e) => {
-            model.send({event: name, data: eventToObject(e)});
-        });
-    });
-}
-
-function registerAllEvents(model, component) {
-    (model.get("_events") || []).forEach(name => {
-        component.$children[0].$on(name, (e) => {
-            model.send({event: name, data: eventToObject(e)});
-        });
-    });
-}
-
-function addListeners(model, component) {
-    const listener = () => {
-        component.$forceUpdate();
-    };
-    const use = (key) =>
-        !key.startsWith("_") && !["v_model"].includes(key);
-
-    model.keys()
-        .filter(use)
-        .forEach(key => model.on("change:"+key, listener));
-
-    model.on("change:v_model", () => {
-        if (model.get("v_model") !== component.v_model) {
-            component.v_model = model.get("v_model");
-        }
-    });
-}
-
-function createAttrsMapping(model) {
-    const useAsAttr = (key) =>
-        model.get(key) !== null && !key.startsWith("_") && !["layout", "children", "slot", "_events", "v_model", "vstyle", "vclass"].includes(key);
-
-    return model.keys()
-        .filter(useAsAttr)
-        .reduce((result, key) => {
-            result[key.replace(/_/g, "-")] = model.get(key);
-            return result;
-        }, {})
-}
-
-function createEventMapping(model) {
-    const mapping = (model.get("_events") || [])
-        .reduce((result, event) => {
-            result[event] = (e) => {
-                model.send({event: event, data: eventToObject(e)});
-                model.save_changes();
-            };
-            return result;
-    }, {});
-    return mapping;
-}
-
-function createContent(model, vm) {
-    return {
-        on: createEventMapping(model),
-        ...model.get("vstyle") && {style: model.get("vstyle")},
-        ...model.get("vclass") && {class: model.get("vclass")},
-        attrs: createAttrsMapping(model),
-        ...model.get("v_model") !== "!!disabled!!" && {
-            model: {
-                value: vm.v_model,
-                callback: (v) => {
-                    model.set("v_model", v === undefined ? null : v);
-                    model.save_changes();
+                model.get("tag"), {
+                    ...model.get("vstyle") && {style: model.get("vstyle")},
+                    ...model.get("vclass") && {class: model.get("vclass")},
+                    ...model.get("attributes"),
+                    ...model.get("slot") && {slot: model.get("slot")}
                 },
-                expression: "v_model"
+                model.get("children").map(child => (typeof child === "string") ? child : this.vueRender(createElement, child)));
+        }
+        const elem = createElement({
+            data() {
+                return {
+                    v_model: model.get("v_model")
+                };
+            },
+            created() {
+                widgetView.addListeners(model, this);
+            },
+            render(createElement) {
+                return createElement(
+                    tag,
+                    widgetView.createContent(model, this),
+                    widgetView.renderChildren(createElement, model, this));
+            }
+        }, {...model.get("slot") && {slot: model.get("slot")}});
+
+        /* Impersonate the wrapped component (e.g. v-tabs uses this name to detect v-tab en v-tab-item) */
+        elem.componentOptions.Ctor.options.name = tag;
+        return elem;
+    }
+
+    eventToObject(event) {
+        let props = undefined;
+        switch (event.constructor) {
+            case MouseEvent:
+                props = ["altKey", "ctrlKey", "metaKey", "shiftKey", "offsetX", "offsetY", "clientX", "clientY", "pageX", "pageY", "screenX", "screenY", "shiftKey", "x", "y"];
+                break;
+            case WheelEvent:
+                props = ["altKey", "ctrlKey", "metaKey", "shiftKey", "offsetX", "offsetY", "clientX", "clientY", "pageX", "pageY", "screenX", "screenY", "shiftKey", "x", "y", "wheelDelta", "wheelDeltaX", "wheelDeltaY"]
+        }
+
+        if (!props) {
+            return event;
+        }
+
+        return props.reduce(
+            (result, key) => {
+                result[key] = event[key];
+                return result},
+            {});
+    }
+
+    registerChangedEvents(model, vueModel) {
+
+        const previous = model.previous("_events") || [];
+        const current = model.get("_events");
+
+        const removed = previous.filter(e => !current.includes(e));
+        const added = current.filter(e => !previous.includes(e));
+
+        removed.forEach(e => vueModel.$children[0].$off(e));
+        added.forEach(name => {
+            vueModel.$children[0].$on(name, (e) => {
+                model.send({event: name, data: this.eventToObject(e)});
+            });
+        });
+    }
+
+    registerAllEvents(model, vueModel) {
+        (model.get("_events") || []).forEach(name => {
+            vueModel.$children[0].$on(name, (e) => {
+                model.send({event: name, data: this.eventToObject(e)});
+            });
+        });
+    }
+
+    addListeners(model, vueModel) {
+        const listener = () => {
+            vueModel.$forceUpdate();
+        };
+        const use = (key) =>
+            !key.startsWith("_") && !["v_model"].includes(key);
+
+        model.keys()
+            .filter(use)
+            .forEach(key => model.on("change:"+key, listener));
+
+        model.on("change:v_model", () => {
+            if (model.get("v_model") !== vueModel.v_model) {
+                vueModel.v_model = model.get("v_model");
+            }
+        });
+    }
+
+    createAttrsMapping(model) {
+        const useAsAttr = (key) =>
+            model.get(key) !== null && !key.startsWith("_") && !["layout", "children", "slot", "_events", "v_model", "vstyle", "vclass"].includes(key);
+
+        return model.keys()
+            .filter(useAsAttr)
+            .reduce((result, key) => {
+                result[key.replace(/_/g, "-")] = model.get(key);
+                return result;
+            }, {})
+    }
+
+    createEventMapping(model) {
+        return (model.get("_events") || [])
+            .reduce((result, event) => {
+                result[event] = (e) => {
+                    model.send({event: event, data: this.eventToObject(e)});
+                    model.save_changes();
+                };
+                return result;
+            }, {});
+    }
+
+    createContent(model, vueModel) {
+        return {
+            on: this.createEventMapping(model),
+            ...model.get("vstyle") && {style: model.get("vstyle")},
+            ...model.get("vclass") && {class: model.get("vclass")},
+            attrs: this.createAttrsMapping(model),
+            ...model.get("v_model") !== "!!disabled!!" && {
+                model: {
+                    value: vueModel.v_model,
+                    callback: (v) => {
+                        model.set("v_model", v === undefined ? null : v);
+                        model.save_changes();
+                    },
+                    expression: "v_model"
+                }
             }
         }
     }
-}
 
-export function renderChildren(component, model, createElement) {
-    if (!component.childCache) {
-        component.childCache = {};
-    }
-    const childViewModels = model.get("children").map(child => {
-        if (typeof (child) === "string") {
-            return child;
-        } else {
-            if (component.childCache[child.cid]) {
-                return component.childCache[child.cid];
+    renderChildren(createElement, model, vueModel) {
+        if (!vueModel.childCache) {
+            vueModel.childCache = {};
+        }
+        const childViewModels = model.get("children").map(child => {
+            if (typeof (child) === "string") {
+                return child;
             } else {
-                const vm = vueRender(createElement, child);
-                component.childCache[child.cid] = vm;
-                return vm;
+                if (vueModel.childCache[child.cid]) {
+                    return vueModel.childCache[child.cid];
+                } else {
+                    const vm = this.vueRender(createElement, child);
+                    vueModel.childCache[child.cid] = vm;
+                    return vm;
+                }
             }
-        }
-    });
+        });
 
-    /* Remove unused components */
-    const childIds = model.get("children").map(child => child.cid);
-    Object.keys(component.childCache)
-        .filter(key => !childIds.includes(key))
-        .forEach(key => delete component.childCache[key]);
-    return childViewModels
+        /* Remove unused components */
+        const childIds = model.get("children").map(child => child.cid);
+        Object.keys(vueModel.childCache)
+            .filter(key => !childIds.includes(key))
+            .forEach(key => delete vueModel.childCache[key]);
+        return childViewModels
+    }
 }
