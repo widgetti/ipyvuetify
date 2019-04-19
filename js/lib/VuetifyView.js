widgetti/ipyvuetify

commit 4aa1e2f62b8d09a3be6860fd548f4758fce68324

Fix naming collision with v-calendar.events

js/lib/VuetifyView.js
@@ -77,8 +77,8 @@ function eventToObject(event) {
 
 function registerChangedEvents(model, component) {
 
-    const previous = model.previous("events") || [];
-    const current = model.get("events");
+    const previous = model.previous("_events") || [];
+    const current = model.get("_events");
 
     const removed = previous.filter(e => !current.includes(e));
     const added = current.filter(e => !previous.includes(e));
@@ -92,7 +92,7 @@ function registerChangedEvents(model, component) {
 }
 
 function registerAllEvents(model, component) {
-    (model.get("events") || []).forEach(name => {
+    (model.get("_events") || []).forEach(name => {
         component.$children[0].$on(name, (e) => {
             model.send({event: name, data: eventToObject(e)});
         });
@@ -119,7 +119,7 @@ function addListeners(model, component) {
 
 function createAttrsMapping(model) {
     const useAsAttr = (key) =>
-        model.get(key) !== null && !key.startsWith("_") && !["layout", "children", "slot", "events", "v_model", "vstyle", "vclass"].includes(key);
+        model.get(key) !== null && !key.startsWith("_") && !["layout", "children", "slot", "_events", "v_model", "vstyle", "vclass"].includes(key);
 
     return model.keys()
         .filter(useAsAttr)
@@ -130,7 +130,7 @@ function createAttrsMapping(model) {
 }
 
 function createEventMapping(model) {
-    const mapping = (model.get("events") || [])
+    const mapping = (model.get("_events") || [])
         .reduce((result, event) => {
             result[event] = (e) => {
                 model.send({event: event, data: eventToObject(e)});

ipyvuetify/Events.py
@@ -18,12 +18,12 @@ class Events:
         if remove and not self._event_handlers_map[event].callbacks:
             del self._event_handlers_map[event]
 
-        difference = set(self._event_handlers_map.keys()) ^ set(self.events)
+        difference = set(self._event_handlers_map.keys()) ^ set(self._events)
         logging.info(f"difference: {difference} current: {self._event_handlers_map.keys()}")
         if len(difference) != 0:
-            self.events = list(self._event_handlers_map.keys())
+            self._events = list(self._event_handlers_map.keys())
 
-        logging.info(self.events)
+        logging.info(self._events)
 
     def fire_event(self, event, data):
         logging.info(f"fir: {self} {event} {data}")

commit ad83abe13bf25f1eb2e12b57fbb5c54e6977412c

Fix: Changes to _events should cause an update.
This broke in 4aa1e2f62b8d09a3be6860fd548f4758fce68324

js/lib/VuetifyView.js
@@ -75,7 +75,7 @@ export class VuetifyView extends VuetifyBaseView {
             vueModel.$forceUpdate();
         };
         const use = (key) =>
-            !key.startsWith("_") && !["v_model"].includes(key);
+            key === "_events" || (!key.startsWith("_") && !["v_model"].includes(key));
 
         model.keys()
             .filter(use)