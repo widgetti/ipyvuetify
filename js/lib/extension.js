4e5792c794ab954e26d31c2eec738a4e81388289
Mario Buikhuizen <mbuikhuizen@gmail.com>
2019-05-15 11:57:50 +0200

    refactor: remove webpack publicPath configuration
    This is unnecessary for the extension module.

--- js/lib/extension.js
+++ js/lib/extension.js
@@ -1,12 +1,6 @@
 // This file contains the javascript that is run when the notebook is loaded.
 // It contains some requirejs configuration and the `load_ipython_extension`
 // which is required for any notebook extension.
-//
-// Some static assets may be required by the custom widget javascript. The base
-// url for the notebook is not known at build time and is therefore computed
-// dynamically.
-const baseUrl = document.querySelector('body').getAttribute('data-base-url');
-__webpack_public_path__ = `${baseUrl}nbextensions/jupyter-vuetify/`; // eslint-disable-line no-undef
 
 // Configure requirejs
 if (window.require) {
