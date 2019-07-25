3647c7ac5466d14019a29fdfd3ad291b084cdb85
Mario Buikhuizen <mariobuikhuizen@gmail.com>
2019-07-25 11:29:27 +0200

    fix: keep render logic backward compatibe for voila-vutify

--- js/src/VuetifyView.js
+++ js/src/VuetifyView.js
@@ -59,4 +59,8 @@ export class VuetifyView extends DOMWidgetView {
             });
         });
     }
+
+    vueRender(createElement) {
+        return vueRender(createElement, this.model, this);
+    }
 }
