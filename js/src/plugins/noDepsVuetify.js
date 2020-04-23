af6b78c4d14e35dd0c8aac03c611f67b2c143443
Mario Buikhuizen <mariobuikhuizen@gmail.com>
2020-04-23 15:58:23 +0200

    feat: add theme support (#64)

---
+++ js/src/plugins/noDepsVuetify.js
@@ -0,0 +1,8 @@
+// import vuetify from 'vuetify';
+
+/* The import above would break voila-vuetify and voila-embed users, so use the workaround below
+ * until all users have upgraded to Voila-vuetify > 0.2.2 and voila-embed > 2020-02-11, or
+ * ipyvuetify bumps a major version */
+const vuetify = window.app && window.app.$vuetify && { framework: window.app.$vuetify };
+
+export default vuetify;
