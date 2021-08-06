widgetti/ipyvuetify

commit ccb9a28a3d069d210e0c40d8e2a15b5c2fc76037

refactor: drop read and readall methods
We can entirely rely on the base class implementation

ipyvuetify/extra/file_input.py
@@ -110,26 +110,6 @@ class ClientSideFile(io.RawIOBase):
 
         return size
 
-    def read(self, size=-1):
-        remaining = self.size - self.offset
-
-        if remaining <= 0:
-            return b''
-
-        if size == -1:
-            num_bytes = remaining
-        else:
-            num_bytes = min(remaining, size)
-
-        buffer = bytearray(num_bytes)
-        self.readinto(buffer)
-
-        return bytes(buffer)
-
-    def readall(self):
-        return self.read()
-
-
 class FileInput(v.VuetifyTemplate):
     template = traitlets.Unicode(load_template('file_input.vue')).tag(sync=True)
     data = traitlets.Unicode('{myfiles: undefined}').tag(sync=True)

commit 2c64abc7da058017bcbd4364696778dd361f214a

fix: performance regression when reading all data in FileInput
Introduced in #111 / ccb9a28a, since a file-like-object doesn't
have a size, the default implementation reads data in 8kb chunks
when no size to read is given. This results in a lot of messages
being send to the front-end, which is very slow.

ipyvuetify/extra/file_input.py
@@ -110,6 +110,10 @@ class ClientSideFile(io.RawIOBase):
 
         return size
 
+    def readall(self):
+        return self.read(self.size - self.offset)
+
+
 class FileInput(v.VuetifyTemplate):
     template = traitlets.Unicode(load_template('file_input.vue')).tag(sync=True)
     data = traitlets.Unicode('{myfiles: undefined}').tag(sync=True)