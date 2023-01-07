3e6e05e99609c2b8f3396bac2a8d9997710f237a
12rambau <pierrick.rambaud49@gmail.com>
2023-01-07 18:57:39 +0100

    docs: change the theme

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
+# -- Path setup ----------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -15,7 +15,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 
-# -- Project information -----------------------------------------------------
+# -- Project information -------------------------------------------------------
 
 project = 'ipyvuetify'
 copyright = '2020, Mario Buikhuizen'
@@ -25,14 +25,13 @@ author = 'Mario Buikhuizen'
 release = '1.2.2'
 
 
-# -- General configuration ---------------------------------------------------
+# -- General configuration -----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'jupyter_sphinx.execute',
-    'sphinx_rtd_theme'
+    'jupyter_sphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,21 +43,43 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-# -- Options for HTML output -------------------------------------------------
+# -- Options for HTML output ---------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
+#html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['custom.css']
 
-master_doc = 'index'
+# -- Theme configuration -------------------------------------------------------
 
-html_css_files = [
-    'custom.css',
-]
+html_theme_options = {
+    "use_edit_page_button": True,
+    "show_prev_next": True,
+    "navbar_start": ["navbar-logo"],
+    "secondary_sidebar_items": ["page-toc.html", "searchbox.html", "edit-this-page.html", "sourcelink.html"],
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/widgetti/ipyvuetify",
+            "icon": "fa-brands fa-github",
+        },
+        {
+            "name": "Pypi",
+            "url": "https://pypi.org/project/ipyvuetify/",
+            "icon": "fa-brands fa-python",
+        },
+    ],
+}
+html_context = {
+    "github_user": "widgetti",
+    "github_repo": "ipyvuetify",
+    "github_version": "master",
+    "doc_path": "docs",
+}
--- docs/index.rst
+++ docs/index.rst
@@ -2,31 +2,9 @@ ipyvuetify: Jupyter widgets based on Vuetify UI components
 ==========================================================
 
 .. toctree::
-    :caption: Introduction
-    :maxdepth: 2
 
     introduction
-
-.. toctree::
-    :caption: Installation
-    :maxdepth: 2
-
     installation
-
-.. toctree::
-    :caption: Usage
-    :maxdepth: 2
-
     usage
-
-.. toctree::
-    :caption: Advanced usage
-    :maxdepth: 2
-
     advanced_usage
-
-.. toctree::
-    :caption: Template usage
-    :maxdepth: 2
-
     template_usage
