59ad7c9d8fda30c4dbf092ceddbe2c6ba523737a
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-02-14 11:27:40 +0100

    fix: create new app when vuetify view is child of a widget for nodeps
    Previously, when we had a vue->widget->vue tree outside of the notebook
    context (in Voila or Solara), we would not create a new Vue app.
    The widget would render as an empty div, and the vue app would not
    be used at all.

--- js/src/nodepsVuetifyView.js
+++ js/src/nodepsVuetifyView.js
@@ -2,14 +2,41 @@ import { DOMWidgetView } from "@jupyter-widgets/base";
 import { vueRender, createViewContext } from "jupyter-vue";
 
 export class VuetifyView extends DOMWidgetView {
-  vueRender(createElement) {
-    return createElement({
-      provide: {
-        viewCtx: createViewContext(this),
-      },
-      render: () => {
-        return vueRender(createElement, this.model, this);
-      },
-    });
-  }
+    remove() {
+        if(this.vueApp) {
+            this.vueApp.$destroy();
+        }
+        return super.remove();
+    }
+
+    render() {
+        super.render();
+        this.displayed.then(() => {
+            const vueEl = document.createElement('div');
+            this.el.appendChild(vueEl);
+            const view = this;
+
+            this.vueApp = new Vue({
+                // TODO: should we pass in vuetify here?
+                el: vueEl,
+                provide: {
+                    viewCtx: createViewContext(this),
+                },
+                render(createElement) {
+                    return view.vueRender(createElement);
+                },
+            });
+        });
+    }
+
+    vueRender(createElement) {
+        return createElement({
+          provide: {
+            viewCtx: createViewContext(this),
+          },
+          render: () => {
+            return vueRender(createElement, this.model, this);
+          },
+        });
+    }
 }
