0b74c4588749fc6de4b8374bade52f576a2230ed
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-02-23 21:14:48 +0100

    chore: cleanup and format setup.py

--- setup.py
+++ setup.py
@@ -1,74 +1,80 @@
 from __future__ import print_function
-from setuptools import setup, find_packages, Command
-from setuptools.command.sdist import sdist
+
+import glob
+import os
+import platform
+import sys
+from distutils import log
+from subprocess import CalledProcessError, check_call
+
+from setuptools import Command, find_packages, setup
 from setuptools.command.build_py import build_py
 from setuptools.command.egg_info import egg_info
-from subprocess import check_call, CalledProcessError
-from distutils import log
-import os
-import sys
-import platform
-import glob
+from setuptools.command.sdist import sdist
 
 here = os.path.dirname(os.path.abspath(__file__))
-node_root = os.path.join(here, 'js')
-is_repo = os.path.exists(os.path.join(here, '.git'))
+node_root = os.path.join(here, "js")
+is_repo = os.path.exists(os.path.join(here, ".git"))
 
-npm_path = os.pathsep.join([
-    os.path.join(node_root, 'node_modules', '.bin'),
-    os.environ.get('PATH', os.defpath),
-])
+npm_path = os.pathsep.join(
+    [
+        os.path.join(node_root, "node_modules", ".bin"),
+        os.environ.get("PATH", os.defpath),
+    ]
+)
 
 
-LONG_DESCRIPTION = 'Jupyter widgets based on vuetify UI components'
+LONG_DESCRIPTION = "Jupyter widgets based on vuetify UI components"
 
 
 def js_prerelease(command, strict=False):
     """decorator for building minified js/css prior to another command"""
+
     class DecoratedCommand(command):
         def run(self):
-            jsdeps = self.distribution.get_command_obj('jsdeps')
+            jsdeps = self.distribution.get_command_obj("jsdeps")
             if not is_repo and all(os.path.exists(t) for t in jsdeps.targets):
                 # sdist, nothing to do
                 command.run(self)
                 return
 
             try:
-                self.distribution.run_command('generate_source')
-                self.distribution.run_command('jsdeps')
+                self.distribution.run_command("generate_source")
+                self.distribution.run_command("jsdeps")
             except Exception as e:
                 missing = [t for t in jsdeps.targets if not os.path.exists(t)]
                 if strict or missing:
-                    log.warn('rebuilding js and css failed')
+                    log.warn("rebuilding js and css failed")
                     if missing:
-                        log.error('missing files: %s' % missing)
+                        log.error("missing files: %s" % missing)
                     raise e
                 else:
-                    log.warn('rebuilding js and css failed (not a problem)')
+                    log.warn("rebuilding js and css failed (not a problem)")
                     log.warn(str(e))
             command.run(self)
             update_package_data(self.distribution)
+
     return DecoratedCommand
 
 
 def update_package_data(distribution):
     """update package_data to catch changes during setup"""
-    build_py = distribution.get_command_obj('build_py')
+    build_py = distribution.get_command_obj("build_py")
     # distribution.package_data = find_package_data()
     # re-init build_py options which load package_data
     build_py.finalize_options()
 
 
 class NPM(Command):
-    description = 'install package.json dependencies using npm'
+    description = "install package.json dependencies using npm"
 
     user_options = []
 
-    node_modules = os.path.join(node_root, 'node_modules')
+    node_modules = os.path.join(node_root, "node_modules")
 
     targets = [
-        os.path.join(here, 'ipyvuetify', 'nbextension', 'extension.js'),
-        os.path.join(here, 'ipyvuetify', 'nbextension', 'index.js')
+        os.path.join(here, "ipyvuetify", "nbextension", "extension.js"),
+        os.path.join(here, "ipyvuetify", "nbextension", "index.js"),
     ]
 
     def initialize_options(self):
@@ -78,16 +84,16 @@ class NPM(Command):
         pass
 
     def get_npm_name(self):
-        npmName = 'npm'
-        if platform.system() == 'Windows':
-            npmName = 'npm.cmd'
+        npmName = "npm"
+        if platform.system() == "Windows":
+            npmName = "npm.cmd"
 
         return npmName
 
     def has_npm(self):
         npmName = self.get_npm_name()
         try:
-            check_call([npmName, '--version'])
+            check_call([npmName, "--version"])
             return True
         except CalledProcessError:
             return False
@@ -98,23 +104,32 @@ class NPM(Command):
     def run(self):
         has_npm = self.has_npm()
         if not has_npm:
-            log.error("`npm` unavailable. If you're running this command using sudo, "
-                      "make sure `npm` is available to sudo")
+            log.error(
+                "`npm` unavailable. If you're running this command using sudo, "
+                "make sure `npm` is available to sudo"
+            )
 
         env = os.environ.copy()
-        env['PATH'] = npm_path
+        env["PATH"] = npm_path
 
         if self.should_run_npm_install():
-            log.info("Installing build dependencies with npm.  This may take a while...")
+            log.info(
+                "Installing build dependencies with npm.  This may take a while..."
+            )
             npmName = self.get_npm_name()
-            check_call([npmName, 'install'], cwd=node_root, stdout=sys.stdout, stderr=sys.stderr)
+            check_call(
+                [npmName, "install"],
+                cwd=node_root,
+                stdout=sys.stdout,
+                stderr=sys.stderr,
+            )
             os.utime(self.node_modules, None)
 
         for t in self.targets:
             if not os.path.exists(t):
-                msg = 'Missing file: %s' % t
+                msg = "Missing file: %s" % t
                 if not has_npm:
-                    msg += '\nnpm is required to build a development version of a widget extension'
+                    msg += "\nnpm is required to build a development version of a widget extension"
                 raise ValueError(msg)
 
         # update package data in case this created new files
@@ -122,7 +137,7 @@ class NPM(Command):
 
 
 class GenerateSource(Command):
-    description = 'Generate source from api specification'
+    description = "Generate source from api specification"
 
     user_options = []
 
@@ -134,62 +149,65 @@ class GenerateSource(Command):
 
     def run(self):
         from generate_source import generate
+
         generate()
 
 
 version_ns = {}
-with open(os.path.join(here, 'ipyvuetify', '_version.py')) as f:
+with open(os.path.join(here, "ipyvuetify", "_version.py")) as f:
     exec(f.read(), {}, version_ns)
 
-setup_args = {
-    'name': 'ipyvuetify',
-    'version': version_ns['__version__'],
-    'description': 'Jupyter widgets based on vuetify UI components',
-    'long_description': LONG_DESCRIPTION,
-    'include_package_data': True,
-    'data_files': [
-        ('share/jupyter/nbextensions/jupyter-vuetify', glob.glob('ipyvuetify/nbextension/*')),
-        ('share/jupyter/labextensions/jupyter-vuetify',
-         glob.glob('ipyvuetify/labextension/package.json')),
-        ('share/jupyter/labextensions/jupyter-vuetify/static',
-         glob.glob('ipyvuetify/labextension/static/*')),
-        ('etc/jupyter/nbconfig/notebook.d', ['jupyter-vuetify.json'])
+setup(
+    name="ipyvuetify",
+    version=version_ns["__version__"],
+    description="Jupyter widgets based on vuetify UI components",
+    long_description=LONG_DESCRIPTION,
+    include_package_data=True,
+    data_files=[
+        (
+            "share/jupyter/nbextensions/jupyter-vuetify",
+            glob.glob("ipyvuetify/nbextension/*"),
+        ),
+        (
+            "share/jupyter/labextensions/jupyter-vuetify",
+            glob.glob("ipyvuetify/labextension/package.json"),
+        ),
+        (
+            "share/jupyter/labextensions/jupyter-vuetify/static",
+            glob.glob("ipyvuetify/labextension/static/*"),
+        ),
+        ("etc/jupyter/nbconfig/notebook.d", ["jupyter-vuetify.json"]),
     ],
-    'install_requires': [
-        'ipyvue>=1.5,<2',
+    install_requires=[
+        "ipyvue>=1.5,<2",
     ],
-    'packages': find_packages(exclude=['generate_source']),
-    'zip_safe': False,
-    'cmdclass': {
-        'build_py': js_prerelease(build_py),
-        'egg_info': js_prerelease(egg_info),
-        'sdist': js_prerelease(sdist, strict=True),
-        'jsdeps': NPM,
-        'generate_source': GenerateSource,
+    packages=find_packages(exclude=["generate_source"]),
+    zip_safe=False,
+    cmdclass={
+        "build_py": js_prerelease(build_py),
+        "egg_info": js_prerelease(egg_info),
+        "sdist": js_prerelease(sdist, strict=True),
+        "jsdeps": NPM,
+        "generate_source": GenerateSource,
     },
-
-    'author': 'Mario Buikhuizen, Maarten Breddels',
-    'author_email': 'mbuikhuizen@gmail.com, maartenbreddels@gmail.com',
-    'url': 'https://github.com/widgetti/ipyvuetify',
-    'keywords': [
-        'ipython',
-        'jupyter',
-        'widgets',
+    author="Mario Buikhuizen, Maarten Breddels",
+    author_email="mbuikhuizen@gmail.com, maartenbreddels@gmail.com",
+    url="https://github.com/widgetti/ipyvuetify",
+    keywords=[
+        "ipython",
+        "jupyter",
+        "widgets",
     ],
-    'classifiers': [
-        'Development Status :: 4 - Beta',
-        'Framework :: IPython',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'Topic :: Multimedia :: Graphics',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: IPython",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Multimedia :: Graphics",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-}
-
-setup(**setup_args)
+)
