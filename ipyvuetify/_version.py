e24101265df17aa90cf756b24289d23c5bcbb4d0
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-04-03 16:47:33 +0200

    chore: setup automated releasing

--- ipyvuetify/_version.py
+++ ipyvuetify/_version.py
@@ -1,14 +1,2 @@
-version_info = (1, 8, 5, "final")
-
-_specifier_ = {"alpha": "a", "beta": "b", "candidate": "rc", "final": ""}
-
-__version__ = "%s.%s.%s%s" % (
-    version_info[0],
-    version_info[1],
-    version_info[2],
-    ""
-    if version_info[3] == "final"
-    else _specifier_[version_info[3]] + str(version_info[4]),
-)
-
+__version__ = "1.8.5"
 semver = "^" + __version__
