11ac604423d632c020433da08e95b6cebc90d769
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2023-04-27 08:40:00 +0200

    buimd: only install npm packages upon egg-info creation

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ from pathlib import Path
 
 from pynpm import NPMPackage
 from setuptools import Command, setup
-from setuptools.command.build_py import build_py
 from setuptools.command.egg_info import egg_info
-from setuptools.command.sdist import sdist
 
 ROOT = Path(__file__).parent
 sys.path.append(str(ROOT))
@@ -57,10 +55,4 @@ def get_data_files():
     ]
 
 
-setup(
-    cmdclass={
-        "build_py": js_prerelease(build_py),
-        "egg_info": js_prerelease(egg_info),
-        "sdist": js_prerelease(sdist),
-    },
-)
+setup(cmdclass={"egg_info": js_prerelease(egg_info)})
