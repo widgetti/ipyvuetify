07149b8e8b2dd7db74188edb52d454460da37342
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2023-08-24 23:21:31 +0200

    refacctor: rollback the conf.py to previous state

--- docs/conf.py
+++ docs/conf.py
@@ -1,30 +1,67 @@
-"""Configuration file for the Sphinx documentation builder.
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-This file only contains a selection of the most common options. For a full
-list see the documentation:
-https://www.sphinx-doc.org/en/master/usage/configuration.html
-"""
+# -- Path setup --------------------------------------------------------------
 
-# -- Project information -------------------------------------------------------
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
 
 project = "ipyvuetify"
 copyright = "2020, Mario Buikhuizen"
 author = "Mario Buikhuizen"
-release = "1.8.10"
 
-# -- General configuration -----------------------------------------------------
+# The full version, including alpha/beta/rc tags
+release = "1.2.2"
 
-extensions = ["jupyter_sphinx"]
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ["jupyter_sphinx", "sphinx_rtd_theme"]
+
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-# -- Options for HTML output ---------------------------------------------------
 
-html_theme = "pydata_sphinx_theme"
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_css_files = ["custom.css"]
 
-# -- Theme configuration -------------------------------------------------------
+master_doc = "index"
+
+html_css_files = [
+    "custom.css",
+]
+
+
+# -- Theme configuration -----------------------------------------------------
 
 html_theme_options = {
     "use_edit_page_button": True,
