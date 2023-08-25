eadb526ad2c91ee7c01e914200cf4b6c3fb20d2e
Rambaud Pierrick <12rambau@users.noreply.github.com>
2023-08-25 12:45:31 +0200

    docs: change the theme (#238)

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,38 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
+
+# -- Theme configuration -----------------------------------------------------
+
+html_theme_options = {
+    "use_edit_page_button": True,
+    "show_prev_next": True,
+    "navbar_start": ["navbar-logo"],
+    "secondary_sidebar_items": [
+        "page-toc.html",
+        "searchbox.html",
+        "edit-this-page.html",
+        "sourcelink.html",
+    ],
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/widgetti/ipyvuetify",
+            "icon": "fa-brands fa-github",
+        },
+        {
+            "name": "Pypi",
+            "url": "https://pypi.org/project/ipyvuetify/",
+            "icon": "fa-brands fa-python",
+        },
+    ],
+}
+html_context = {
+    "github_user": "widgetti",
+    "github_repo": "ipyvuetify",
+    "github_version": "master",
+    "doc_path": "docs",
+}
