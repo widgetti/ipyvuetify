c13edb7a13907ef0d7ddad7cf9bc66c0ccb57bd1
Mario Buikhuizen <mariobuikhuizen@widgetti.io>
2023-11-15 17:28:29 +0100

    feat: add theme support

--- ipyvuetify/Themes.py
+++ ipyvuetify/Themes.py
@@ -8,10 +8,16 @@ class Themes:
     def __init__(self):
         self.light = ThemeColors(
             _theme_name="light",
-            primary="#1976D2",
-            secondary="#424242",
-            accent="#82B1FF",
-            error="#FF5252",
+            background="#FFFFFF",
+            surface="#FFFFFF",
+            surface_bright="#FFFFFF",
+            surface_variant="#424242",
+            on_surface_variant="#EEEEEE",
+            primary="#6200EE",
+            primary_darken_1="#3700B3",
+            secondary="#03DAC6",
+            secondary_darken_1="#018786",
+            error="#B00020",
             info="#2196F3",
             success="#4CAF50",
             warning="#FB8C00",
@@ -19,10 +25,16 @@ class Themes:
 
         self.dark = ThemeColors(
             _theme_name="dark",
-            primary="#2196F3",
-            secondary="#424242",
-            accent="#FF4081",
-            error="#FF5252",
+            background="#121212",
+            surface="#212121",
+            surface_bright="#ccbfd6",
+            surface_variant="#a3a3a3",
+            on_surface_variant="#424242",
+            primary="#BB86FC",
+            primary_darken_1="#3700B3",
+            secondary="#03DAC5",
+            secondary_darken_1="#03DAC5",
+            error="#CF6679",
             info="#2196F3",
             success="#4CAF50",
             warning="#FB8C00",
@@ -48,6 +60,14 @@ class Theme(Widget):
         self.themes = Themes()
 
 
+class ColorNotAvailable(Unicode):
+    def get(self, *ignored):
+        raise AttributeError(f"The theme color '{self.name}' is no longer available in Vuetify 3")
+
+    def set(self, *ignored):
+        raise AttributeError(f"The theme color '{self.name}' is no longer available in Vuetify 3")
+
+
 class ThemeColors(Widget):
 
     _model_name = Unicode("ThemeColorsModel").tag(sync=True)
@@ -60,14 +80,21 @@ class ThemeColors(Widget):
 
     _theme_name = Unicode().tag(sync=True)
 
+    accent = ColorNotAvailable()
+    anchor = ColorNotAvailable()
+    background = Unicode().tag(sync=True)
+    surface = Unicode().tag(sync=True)
+    surface_bright = Unicode().tag(sync=True)
+    surface_variant = Unicode().tag(sync=True)
+    on_surface_variant = Unicode().tag(sync=True)
     primary = Unicode().tag(sync=True)
+    primary_darken_1 = Unicode().tag(sync=True)
     secondary = Unicode().tag(sync=True)
-    accent = Unicode().tag(sync=True)
+    secondary_darken_1 = Unicode().tag(sync=True)
     error = Unicode().tag(sync=True)
     info = Unicode().tag(sync=True)
     success = Unicode().tag(sync=True)
     warning = Unicode().tag(sync=True)
-    anchor = Unicode(None, allow_none=True).tag(sync=True)
 
 
 theme = Theme()
--- js/src/VuetifyView.js
+++ js/src/VuetifyView.js
@@ -1,10 +1,12 @@
 import * as Vue from "vue"; // eslint-disable-line import/no-extraneous-dependencies
 import { VueView, createViewContext, vueRender } from "jupyter-vue";
 import "vuetify/styles";
-import { createVuetify } from "vuetify";
+import colors from "vuetify/lib/util/colors.mjs";
+import { createVuetify, useTheme } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import { VDataTable } from "vuetify/labs/VDataTable";
+import { ThemeColorsModel, ThemeModel } from "./Themes";
 
 const observer = new MutationObserver((mutationList, observer) => {
   for (const mutation of mutationList) {
@@ -24,11 +26,6 @@ observer.observe(document.body, { childList: true });
 
 export class VuetifyView extends VueView {
   addPlugins(vueApp) {
-    console.log(
-      Vue.compile(
-        `<v-btn v-model="xx" :color="'primary'" :key="cc">test</v-btn>`
-      )
-    );
     super.addPlugins(vueApp);
     const vuetify = createVuetify({
       components: { ...components, VDataTable },
@@ -37,6 +34,28 @@ export class VuetifyView extends VueView {
     this.el.classList.add("vuetify-styles");
     document.querySelector("html").style.fontSize = "16px";
     vueApp.use(vuetify);
+  }
+
+  async beforeViewRender() {
+    await super.beforeViewRender();
+    const models = await Promise.all(
+      Object.values(this.model.widget_manager._models)
+    );
+    this.themeModel = models.find((m) => m instanceof ThemeModel);
+    models
+      .filter((m) => m instanceof ThemeColorsModel)
+      .forEach((m) => {
+        if (m.get("_theme_name") === "light") {
+          this.themeLightModel = m;
+        } else if (m.get("_theme_name") === "dark") {
+          this.themeDarkModel = m;
+        }
+      });
+  }
+
+  onSetup() {
+    super.onSetup();
+    this.setupTheme();
   }
 
   /* used in pages using nodeps */
@@ -50,4 +69,94 @@ export class VuetifyView extends VueView {
       },
     });
   }
+
+  setupTheme() {
+    if (!this.themeModel) {
+      return;
+    }
+    this.theme = useTheme();
+
+    if (ThemeModel.themeManager) {
+      const setAutoTheme = () => {
+        if (this.themeModel.get("dark") === null) {
+          const isDark = document.body.dataset.jpThemeLight === "false";
+          this.theme.global.name.value = isDark ? "dark" : "light";
+          this.themeModel.set("dark_jlab", isDark);
+          this.themeModel.save_changes();
+        }
+      };
+      ThemeModel.themeManager.themeChanged.connect(() => {
+        setAutoTheme();
+      }, this);
+      setAutoTheme();
+    }
+
+    const onDark = () => {
+      this.theme.global.name.value = this.themeModel.get("dark")
+        ? "dark"
+        : "light";
+    };
+
+    const onColorsLight = () => {
+      this.theme.themes.value.light.colors = getColors(this.themeLightModel);
+    };
+    onColorsLight();
+
+    const onColorsDark = () => {
+      this.theme.themes.value.dark.colors = getColors(this.themeDarkModel);
+    };
+    onColorsDark();
+
+    if (this.themeModel.get("dark") !== null) {
+      onDark();
+    } else if (document.body.dataset.jpThemeLight) {
+      const isDark = document.body.dataset.jpThemeLight === "false";
+      this.theme.global.name.value = isDark ? "dark" : "light";
+      this.themeModel.set("dark_jlab", isDark);
+      this.themeModel.save_changes();
+    } else if (document.body.classList.contains("theme-dark")) {
+      this.theme.global.name.value = "dark";
+      this.themeModel.set("dark", true);
+      this.themeModel.save_changes();
+    } else if (document.body.classList.contains("theme-light")) {
+      this.themeModel.set("dark", false);
+      this.themeModel.save_changes();
+    }
+
+    Vue.onMounted(() => {
+      this.themeModel.on("change:dark", onDark);
+      this.themeLightModel.on("change", onColorsLight);
+      this.themeDarkModel.on("change", onColorsDark);
+    });
+
+    Vue.onUnmounted(() => {
+      this.themeModel.off("change:dark", onDark);
+      this.themeLightModel.off("change", onColorsLight);
+      this.themeDarkModel.off("change", onColorsDark);
+    });
+  }
+}
+
+function parseColor(colorStr) {
+  const parts = colorStr.split(".").slice(1);
+  let result = colors;
+
+  parts.forEach((part) => {
+    result = result[part];
+  });
+
+  return typeof result === "string" ? result : result.base;
+}
+
+function getColors(colorModel) {
+  return _.mapKeys(
+    _.mapValues(
+      _.pickBy(
+        { ...colorModel.attributes },
+        (v, k) => !k.startsWith("_") && k !== "accent" && k !== "anchor"
+      ),
+      (v) => (v.startsWith("colors.") ? parseColor(v) : v)
+    ),
+    (v, k) => k.replace(/_/g, "-")
+  );
 }
