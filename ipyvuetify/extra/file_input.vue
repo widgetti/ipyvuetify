db0c68702e5de9e6d28aacb3b83a9d268736f791
Egor Makarenko <egormkn@yandex.ru>
2023-12-21 00:47:36 +0500

    feat: add drag-and-drop support [draft]

--- ipyvuetify/extra/file_input.vue
+++ ipyvuetify/extra/file_input.vue
@@ -1,5 +1,6 @@
 <template>
   <v-file-input
+    ref="file-input"
     :class="class_ || []"
     :style="style_ || {}"
     v-bind="props"
@@ -30,7 +31,119 @@ module.exports = {
   created() {
     this.chunk_size = 2 * 1024 * 1024;
   },
+  mounted() {
+    const fileInput = this.$refs["file-input"];
+    const input = fileInput.$refs["input"];
+    const inputSlot = fileInput.$refs["input-slot"];
+
+    // Native drag & drop on input[type=file] seems to be supported in Chrome and Firefox since at least 2011:
+    // - https://bugzilla.mozilla.org/show_bug.cgi?id=50660
+    // - https://bugs.webkit.org/show_bug.cgi?id=58401
+
+    input.classList.add("v-file-input-native-dropzone");
+
+    input.addEventListener("click", (e) => {
+      // Disable click propagation to label
+      e.stopPropagation();
+    });
+
+    inputSlot.addEventListener("dragover", (e) => {
+      // Enable custom drag-and-drop behaviour for any target except input
+      if (e.target !== input) e.preventDefault();
+      e.dataTransfer.dropEffect = "copy";
+    });
+
+    inputSlot.addEventListener("dragenter", (e) => {
+      if (e.target !== input) e.preventDefault();
+      if (!inputSlot.contains(e.relatedTarget)) {
+        inputSlot.classList.add("v-file-input-custom-dropzone");
+      }
+    });
+
+    inputSlot.addEventListener("dragleave", (e) => {
+      if (e.target !== input) e.preventDefault();
+      if (!inputSlot.contains(e.relatedTarget)) {
+        inputSlot.classList.remove("v-file-input-custom-dropzone");
+      }
+    });
+
+    // const supportsFileSystemAccessAPI =
+    //   "getAsFileSystemHandle" in DataTransferItem.prototype;
+
+    const supportsFileSystemAccessAPI = false;
+
+    const dropHandler = async (e) => {
+      if (e.target !== input) e.preventDefault();
+
+      e.stopPropagation();
+
+      inputSlot.classList.remove("v-file-input-custom-dropzone");
+
+      let files;
+      if (e.dataTransfer.items) {
+        const fileDataTransferItems = [...e.dataTransfer.items].filter(
+          (item) => item.kind === "file" // `file` misleadingly means actual file or directory
+        );
+
+        let getFiles;
+        let hasDirectories;
+        if (supportsFileSystemAccessAPI) {
+          const fileSystemHandles = await Promise.all(
+            fileDataTransferItems.map((item) => item.getAsFileSystemHandle())
+          );
+
+          hasDirectories = fileSystemHandles.some(
+            (handle) => handle.kind === "directory"
+          );
+
+          getFiles = async () => this.getFileList([]); // FIXME: get FileList from FileSystemHandles
+        } else {
+          const fileSystemEntries = fileDataTransferItems.map((item) =>
+            item.webkitGetAsEntry()
+          );
+
+          hasDirectories = fileSystemEntries.some((entry) => entry.isDirectory);
+
+          getFiles = async () => this.getFileList(fileSystemEntries);
+        }
+
+        if (
+          (!this.webkitdirectory && hasDirectories) ||
+          (!this.multiple && hasDirectories) ||
+          (!this.multiple && fileDataTransferItems.length > 1)
+        ) {
+          if (e.target === input) e.preventDefault(); // Skip native drag-and-drop handler
+          return;
+        }
+
+        if (e.target === input) return; // Use native drag-and-drop handler
+
+        if (
+          !hasDirectories &&
+          e.dataTransfer.files &&
+          e.dataTransfer.files.length >= fileDataTransferItems.length
+        ) {
+          files = e.dataTransfer.files;
+        } else {
+          files = await getFiles();
+        }
+      } else {
+        files = e.dataTransfer.files;
+      }
+
+      files = this.filterAcceptedFiles(files);
+
+      input.files = files;
+      input.dispatchEvent(new Event("change"));
+    };
+
+    input.addEventListener("drop", dropHandler);
+    inputSlot.addEventListener("drop", dropHandler);
+  },
   computed: {
+    webkitdirectory: function () {
+      return "webkitdirectory" in (this.attributes || {});
+    },
     computedLoadingValue: function () {
       return (
         (!this.progress_indeterminate || this.total_progress > 0) &&
@@ -127,6 +240,85 @@ module.exports = {
     },
   },
   methods: {
+    getFileList: async function (fileEntries) {
+      async function readDirectoryEntry(directoryEntry) {
+        const directoryReader = directoryEntry.createReader();
+        const result = [];
+        let entries;
+        do {
+          try {
+            // readEntries will return only some of the entries in a directory
+            // e.g. Chrome returns at most 100 entries at a time
+            entries = await new Promise((resolve, reject) => {
+              directoryReader.readEntries(resolve, reject);
+            });
+          } catch (error) {
+            console.error(error);
+            entries = [];
+          }
+          result.push(...entries);
+        } while (entries.length > 0);
+        return result;
+      }
+
+      const entries = [];
+
+      const queue = [...fileEntries];
+
+      while (queue.length > 0) {
+        let entry = queue.shift();
+        if (entry.isFile) {
+          entries.push(entry);
+        } else if (entry.isDirectory) {
+          queue.push(...(await readDirectoryEntry(entry)));
+        }
+      }
+
+      const files = await Promise.all(
+        entries.map(
+          (e) => new Promise((resolve, reject) => e.file(resolve, reject))
+        )
+      );
+
+      const dt = new DataTransfer();
+      files.forEach((file) => {
+        dt.items.add(file);
+      });
+
+      return dt.files;
+    },
+    isAcceptedFile(file) {
+      if (!this.accept) return true;
+      for (const mime of this.accept.split(/\s*,\s*/)) {
+        if (
+          mime === file.type ||
+          (/^\.[^/\\]+$/.test(mime) && file.name.endsWith(mime)) ||
+          (/^[^/\\]+\/\*$/.test(mime) &&
+            file.type.startsWith(mime.slice(0, -1)))
+        )
+          return true;
+      }
+      return false;
+    },
+    filterAcceptedFiles(files) {
+      const acceptedFiles = [...files].filter(this.isAcceptedFile);
+      if (acceptedFiles.length < files.length) {
+        const dataTransfer = new DataTransfer();
+        for (const file of acceptedFiles) {
+          dataTransfer.items.add(file);
+          if (!this.multiple) break;
+        }
+        return dataTransfer.files;
+      }
+      return files;
+    },
+    change(e) {
+      if (!e.target) return;
+
+      const files = e.target.files || [];
+
+      this.setFiles(this.multiple ? [...files] : files[0]);
+    },
     clear() {
       this.native_file_info = [];
       this.file_info = [];
@@ -179,3 +371,25 @@ module.exports = {
   },
 };
 </script>
+
+<style>
+.v-file-input input[type="file"].v-file-input-native-dropzone {
+  position: absolute;
+  max-width: none;
+  max-height: none;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  opacity: 0;
+  pointer-events: auto;
+}
+
+.v-file-input .v-file-input-custom-dropzone {
+  outline-style: dashed;
+}
+</style>
