4ae4ab778a6c5bb5a174ce5d6dc55e1b69f44d2a
Iisakki Rotko <iisakki.rotko@gmail.com>
2025-02-24 11:26:43 +0100

    feat: export Vue and Vuetify

--- js/src/index.js
+++ js/src/index.js
@@ -2,6 +2,8 @@ import "typeface-roboto";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 import "@mdi/font/css/materialdesignicons.css";
 import "./styles.css";
+import * as Vue from "vue";
+import * as Vuetify from "vuetify";
 
 export { VuetifyView } from "./VuetifyView";
 export * from "./Widgets";
@@ -10,3 +12,4 @@ export { VuetifyTemplateModel } from "./VuetifyTemplate";
 export { ThemeModel, ThemeColorsModel } from "./Themes";
 
 export { version } from "./version";
+export { Vue, Vuetify };
