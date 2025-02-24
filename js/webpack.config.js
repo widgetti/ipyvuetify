3377ce28aca867b9ce9347253f8e33cd0e437f7b
Iisakki Rotko <iisakki.rotko@gmail.com>
2025-02-24 10:57:41 +0100

    feat: scope vuetify styles with :where
    Ports the changes made in https://github.com/widgetti/ipyvuetify/pull/327 to version 3 alpha

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,5 +1,6 @@
 var path = require("path");
 var version = require("./package.json").version;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // Custom webpack rules are generally the same for all webpack bundles, hence
 // stored in a separate local variable.
@@ -100,7 +101,6 @@ module.exports = [
       "@jupyter-widgets/base",
       "@jupyterlab/apputils",
       "jupyter-vue",
-      "@mariobuikhuizen/vuetify/dist/vuetify.min.css",
       "material-design-icons-iconfont",
       "typeface-roboto",
       "@mdi/font",
@@ -124,7 +124,6 @@ module.exports = [
       "@jupyter-widgets/base",
       "@jupyterlab/apputils",
       "jupyter-vue",
-      "@mariobuikhuizen/vuetify/dist/vuetify.min.css",
       "material-design-icons-iconfont",
       "typeface-roboto",
       "@mdi/font",
@@ -156,8 +155,23 @@ module.exports = [
     },
     devtool: "source-map",
     module: {
-      rules,
+      rules: [
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          type: "asset/resource",
+        },
+      ],
     },
+    // we extract the CSS to a separate file, since we might want to import it separately for global use
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "jupyter-vuetify.min.css",
+      }),
+    ],
     externals: ["@jupyter-widgets/base", "@jupyterlab/apputils", "jupyter-vue"],
     resolve: {
       alias: {
