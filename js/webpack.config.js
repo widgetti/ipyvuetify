b0724ee282c2f78ed1897ab3afbd5dfd92a79e4c
Mario Buikhuizen <mariobuikhuizen@widgetti.io>
2026-03-05 14:54:37 +0100

    fix: get the right vue export for nodeps for solara

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -72,6 +72,11 @@ module.exports = [
     module: {
       rules: rules,
     },
+    resolve: {
+      alias: {
+        vue$: path.resolve(__dirname, "lib", "vue_alias"),
+      },
+    },
     externals: [
       "@jupyter-widgets/base",
       "@jupyterlab/apputils",
@@ -94,6 +99,11 @@ module.exports = [
     devtool: "source-map",
     module: {
       rules: rules,
+    },
+    resolve: {
+      alias: {
+        vue$: path.resolve(__dirname, "lib", "vue_alias"),
+      },
     },
     externals: [
       "@jupyter-widgets/base",
